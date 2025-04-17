cfc3cc25df91245344558146ea9426fbcaf454c4
Boy at Tacx <71146761+boyattacx@users.noreply.github.com>
2025-04-17 09:40:51 +0200

    [SA-23869] Allow using pantomime through SPM (#2)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Pantomime"
+    name: "Pantomime",
+    platforms: [.macOS(.v12), .iOS(.v16)],
+    products: [
+        .library(
+            name: "Pantomime",
+            targets: ["Pantomime"])
+    ],
+    targets: [
+        .target(name: "Pantomime")
+    ]
 )
